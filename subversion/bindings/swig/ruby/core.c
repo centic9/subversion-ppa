11474344de987ba5755050d265d8acc84d202bdc
Dominik Stadler <dominik.stadler@gmx.at>
2012-12-23 10:12:39 +0100

    Imported Upstream version 1.7.8

--- subversion/bindings/swig/ruby/core.c
+++ subversion/bindings/swig/ruby/core.c
@@ -37520,15 +37520,15 @@ SWIGEXPORT void Init_core(void) {
   rb_define_module_function(mCore, "svn_pool_create", _wrap_svn_pool_create, -1);
   rb_define_const(mCore, "SVN_VER_MAJOR", SWIG_From_int((int)(1)));
   rb_define_const(mCore, "SVN_VER_MINOR", SWIG_From_int((int)(7)));
-  rb_define_const(mCore, "SVN_VER_PATCH", SWIG_From_int((int)(7)));
-  rb_define_const(mCore, "SVN_VER_MICRO", SWIG_From_int((int)(7)));
+  rb_define_const(mCore, "SVN_VER_PATCH", SWIG_From_int((int)(8)));
+  rb_define_const(mCore, "SVN_VER_MICRO", SWIG_From_int((int)(8)));
   rb_define_const(mCore, "SVN_VER_LIBRARY", SWIG_From_int((int)(1)));
-  rb_define_const(mCore, "SVN_VER_TAG", SWIG_FromCharPtr(" (r1393599)"));
+  rb_define_const(mCore, "SVN_VER_TAG", SWIG_FromCharPtr(" (r1419691)"));
   rb_define_const(mCore, "SVN_VER_NUMTAG", SWIG_FromCharPtr(""));
-  rb_define_const(mCore, "SVN_VER_REVISION", SWIG_From_int((int)(1393599)));
-  rb_define_const(mCore, "SVN_VER_NUM", SWIG_FromCharPtr("1.7.7"));
-  rb_define_const(mCore, "SVN_VER_NUMBER", SWIG_FromCharPtr("1.7.7"));
-  rb_define_const(mCore, "SVN_VERSION", SWIG_FromCharPtr("1.7.7 (r1393599)"));
+  rb_define_const(mCore, "SVN_VER_REVISION", SWIG_From_int((int)(1419691)));
+  rb_define_const(mCore, "SVN_VER_NUM", SWIG_FromCharPtr("1.7.8"));
+  rb_define_const(mCore, "SVN_VER_NUMBER", SWIG_FromCharPtr("1.7.8"));
+  rb_define_const(mCore, "SVN_VERSION", SWIG_FromCharPtr("1.7.8 (r1419691)"));
   
   SwigClassSvn_version_t.klass = rb_define_class_under(mCore, "Svn_version_t", rb_cObject);
   SWIG_TypeClientData(SWIGTYPE_p_svn_version_t, (void *) &SwigClassSvn_version_t);
