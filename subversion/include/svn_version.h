469859e9e33ea4999b81bdded43ee0b6de4c118b
Dominik Stadler <dominik.stadler@gmx.at>
2016-04-28 23:02:12 +0200

    Imported Upstream version 1.8.16

--- subversion/include/svn_version.h
+++ subversion/include/svn_version.h
@@ -70,7 +70,7 @@ extern "C" {
  *
  * @since New in 1.1.
  */
-#define SVN_VER_PATCH      15
+#define SVN_VER_PATCH      16
 
 
 /** @deprecated Provided for backward compatibility with the 1.0 API. */
@@ -93,7 +93,7 @@ extern "C" {
  *
  * Always change this at the same time as SVN_VER_NUMTAG.
  */
-#define SVN_VER_TAG        " (r1718365)"
+#define SVN_VER_TAG        " (r1740329)"
 
 
 /** Number tag: a string describing the version.
@@ -119,7 +119,7 @@ extern "C" {
  * When rolling a tarball, we automatically replace it with what we
  * guess to be the correct revision number.
  */
-#define SVN_VER_REVISION   1718365
+#define SVN_VER_REVISION   1740329
 
 
 /* Version strings composed from the above definitions. */
